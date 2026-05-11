430d4a32b0e85d0f30d9ddc3bb21d9b14083c56c
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2026-05-11 19:22:43 -0300

    first commit

---
+++ src/main/java/com/adssenai/arquiteturalimpa/ArquiteturalimpaApplication.java
@@ -0,0 +1,13 @@
+package com.adssenai.arquiteturalimpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturalimpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturalimpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adssenai/arquiteturalimpa/ArquiteturalimpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adssenai.arquiteturalimpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturalimpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
